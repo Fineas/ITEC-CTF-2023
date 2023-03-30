7802e2032b323760adeffd1fedb413df986591cc
Fineas <finymelly@gmail.com>
2023-03-31 00:41:38 +0300

    finish crypto3

--- o_scrisoare_pierduta/chal/scrisoare.py
+++ o_scrisoare_pierduta/chal/scrisoare.py
@@ -1,5 +1,9 @@
 #!/usr/local/bin/python3
 
+import os
+
+flag = os.environ['FLAG']
+
 def afis():
     print("""
     .-----------.
@@ -13,27 +17,20 @@ def afis():
     """)
 
 
-def meniu():
-    print()
-    print("[1] Ascunde mesaj")
-    print("[2] Verifica mesaj")
-
-
 def ascunde(data1, data2):
     print('!!',data1)
     print('!!',data2)
         
-    plaintext = data1
-    plaintext_length = len(plaintext)
-    assert(plaintext_length < 3, "Eroare lungime mesaj")
+    plaintext_length = len(data1)
+    assert (plaintext_length >= 3), "Eroare lungime mesaj"
 
     if plaintext_length % 3 != 0 :
         if 3 - (plaintext_length % 3) == 2 :
-            plaintext.append(0)
-            plaintext.append(0)
-            plaintext_length = plaintext_length + 2
+            data1.append(0)
+            data1.append(0)
+            data1 = plaintext_length + 2
         else:
-            plaintext.append(0)
+            data1.append(0)
             plaintext_length = plaintext_length + 1
 
     complete_plaintext = []
@@ -46,12 +43,12 @@ def ascunde(data1, data2):
     complete_plaintext.append(19)
     complete_plaintext.append(19)
     complete_plaintext.append(19)
-    for i in plaintext:
+    for i in data1:
         complete_plaintext.append(ord(i))
     plaintext_length = plaintext_length + 9
 
     key = data2
-    assert(len(key) != 9, "Eroare lungime cheie");
+    assert (len(key) == 9), "Eroare lungime cheie"
 
     a11 = ord(key[0])
     a12 = ord(key[1])
@@ -89,7 +86,7 @@ def distribuie(data):
     return distribuit
 
 def verifica(data):
-    if ascunde(data) == [19, 16, 17, 24, 22, 8, 5, 15, \
+    if data == [19, 16, 17, 24, 22, 8, 5, 15, \
                          16, 15, 4, 21, 1, 9, 18, 21, \
                          25, 0, 1, 19, 24, 25, 6, 11, \
                          22, 24, 24, 19, 13, 12, 13, \
@@ -98,9 +95,9 @@ def verifica(data):
                          10, 15, 11, 8, 7, 12, 3, 1, \
                          23, 22, 5, 10, 9, 24, 3, 23, \
                          9, 25, 18, 19, 12, 1, 11, 0, 19, 23, 16, 3]:
-        return "ITEC{redacted}"
+        return flag
     else:
-        return "[*] Interzis cetatenilor turmentate!"
+        return "Interzis cetatenilor turmentate!"
 
 if __name__ == "__main__":
 
@@ -108,38 +105,4 @@ if __name__ == "__main__":
 
     mesaj = input(" Introduceti mesajul: ")
     cheie = input(" Introduceti cheia: ")
-    print("[*]",  verifica(ascunde(distribuie(mesaj), distribuie(cheie)))
-
-    
-
-'''
-DICTIONARY
-A   B   C   D   E   F   G   H   I   J   K   L   M   N   O   P   Q   R   S   T   U   V   W   X   Y   Z
-0   1   2   3   4   5   6   7   8   9  10  11  12  13  14  15  16  17  18  19  20  21  22  23  24  25  
-
-LEAK
-EPLANETTT
-4  = E
-15 = P
-11 = L
-0  = A
-13 = N
-4  = E
-19 = T
-19 = T
-19 = T
-
-KEY
-ZLGKNZMTC
-Z = 25
-L = 11
-G = 6
-K = 10
-N = 13
-Z = 25
-M = 12
-T = 19
-C = 2
-
-DRAGAMEACECILIALOVEYOUCECILIANEVEDEMLARASCRUCEDEDRUMINMIEZDENOAPTE
-'''
+    print("[*] ",  verifica(ascunde(distribuie(mesaj), distribuie(cheie))))
